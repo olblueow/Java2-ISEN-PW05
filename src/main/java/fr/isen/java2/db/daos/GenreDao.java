d92e6d7d1104f11854ac7bb548fc6057e028d72d
Olivier CLAVIER <clavieroliviermarc@outlook.com>
2026-02-08 13:25:47 +0100

    feat: implement GenreDao with JDBC (list, get and add)

--- src/main/java/fr/isen/java2/db/daos/GenreDao.java
+++ src/main/java/fr/isen/java2/db/daos/GenreDao.java
@@ -1,5 +1,10 @@
 package fr.isen.java2.db.daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.isen.java2.db.entities.Genre;
@@ -7,14 +12,56 @@ import fr.isen.java2.db.entities.Genre;
 public class GenreDao {
 
 	public List<Genre> listGenres() {
-		throw new RuntimeException("Method is not yet implemented");
+		List<Genre> listOfGenres = new ArrayList<>();
+
+		try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+			try(Statement statement = connection.createStatement()) {
+				try(ResultSet results = statement.executeQuery("SELECT * FROM genre")) {
+					while (results.next()){
+						Genre genre = new Genre(
+							results.getInt("idgenre"), 
+							results.getString("name"));
+						listOfGenres.add(genre);
+					}
+				}
+			}
+			return listOfGenres;
+		} catch (Exception e) {
+			// Manage exception 
+			throw new RuntimeException("Error while listing genres", e);
+		}
 	}
 
 	public Genre getGenre(String name) {
-		throw new RuntimeException("Method is not yet implemented");
+		try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+			try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM genre WHERE name = ?")) {
+				statement.setString(1, name);
+				try (ResultSet results = statement.executeQuery()) {
+					if (results.next()){
+						return new Genre(
+							results.getInt("idgenre"), 
+							results.getString("name"));
+					} else {
+						return null;
+					}
+				}
+			}
+		} catch (Exception e) {
+			// Manage exception 
+			throw new RuntimeException("Error while getting genre", e);
+		}
 	}
 
 	public void addGenre(String name) {
-		throw new RuntimeException("Method is not yet implemented");
+		try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+			String sqlQuery = "INSERT INTO genre(name) VALUES(?)";
+			try(PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
+				statement.setString(1, name);
+				statement.executeUpdate();
+			}
+		} catch (Exception e) {
+			// Manage exception 
+			throw new RuntimeException("Error while adding genre", e);
+		}
 	}
 }
