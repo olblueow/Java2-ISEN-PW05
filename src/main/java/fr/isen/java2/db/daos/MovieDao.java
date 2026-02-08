ddb2b487420ba4e77e6145aa9c49f6e8162ed3e2
Olivier CLAVIER <clavieroliviermarc@outlook.com>
2026-02-08 13:11:44 +0100

    feat: implement MovieDao with JDBC (list, filter by genre, and add)

--- src/main/java/fr/isen/java2/db/daos/MovieDao.java
+++ src/main/java/fr/isen/java2/db/daos/MovieDao.java
@@ -1,20 +1,90 @@
 package fr.isen.java2.db.daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+import fr.isen.java2.db.entities.Genre;
 import fr.isen.java2.db.entities.Movie;
 
 public class MovieDao {
 
 	public List<Movie> listMovies() {
-		throw new RuntimeException("Method is not yet implemented");
+		List<Movie> listOfMovies = new ArrayList<>();
+
+		try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+			try(Statement statement = connection.createStatement()) {
+				try(ResultSet results = statement.executeQuery("SELECT * FROM movie JOIN genre ON movie.genre_id = genre.idgenre")) {
+					while (results.next()){
+						Movie movie = new Movie(results.getInt("idmovie"),
+								results.getString("title"),
+								results.getDate("release_date").toLocalDate(),
+								new Genre(results.getInt("idgenre"), 
+								results.getString("name")),
+								results.getInt("duration"),
+								results.getString("director"),
+								results.getString("summary"));
+						listOfMovies.add(movie);
+					}
+				}
+			}
+			return listOfMovies;
+		} catch (Exception e) {
+			// Manage exception 
+			throw new RuntimeException("Error while listing movies", e);
+		}
 	}
 
 	public List<Movie> listMoviesByGenre(String genreName) {
-		throw new RuntimeException("Method is not yet implemented");
+		List<Movie> listOfMovies = new ArrayList<>();
+
+		try (Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+			try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM movie JOIN genre ON movie.genre_id = genre.idgenre WHERE genre.name = ?")) {
+				statement.setString(1, genreName);
+				try (ResultSet results = statement.executeQuery()) {
+					while (results.next()){
+						Movie movie = new Movie(results.getInt("idmovie"),
+								results.getString("title"),
+								results.getDate("release_date").toLocalDate(),
+								new Genre(results.getInt("idgenre"), 
+								results.getString("name")),
+								results.getInt("duration"),
+								results.getString("director"),
+								results.getString("summary"));
+						listOfMovies.add(movie);
+					}
+				}
+			}
+			return listOfMovies;
+		} catch (Exception e) {
+			// Manage exception
+			throw new RuntimeException("Error while listing movies by genre", e);
+		}
 	}
 
 	public Movie addMovie(Movie movie) {
-		throw new RuntimeException("Method is not yet implemented");
-	}
+		try(Connection connection = DataSourceFactory.getDataSource().getConnection()) {
+			String sqlQuery = "INSERT INTO movie(title,release_date,genre_id,duration,director,summary) VALUES(?,?,?,?,?,?)";
+			try (PreparedStatement statement = connection.prepareStatement(sqlQuery,Statement.RETURN_GENERATED_KEYS)){
+				statement.setString(1, movie.getTitle());
+				statement.setDate(2, java.sql.Date.valueOf(movie.getReleaseDate()));
+				statement.setInt(3, movie.getGenre().getId());
+				statement.setInt(4, movie.getDuration());
+				statement.setString(5, movie.getDirector());
+				statement.setString(6, movie.getSummary());
+				statement.executeUpdate();
+				ResultSet ids = statement.getGeneratedKeys();
+					if (ids.next()) {
+						return new Movie(ids.getInt(1), movie.getTitle(), movie.getReleaseDate(), movie.getGenre(), movie.getDuration(), movie.getDirector(), movie.getSummary());
+					}
+			}
+		}catch (Exception e) {
+			// Manage exception
+			throw new RuntimeException("Error while adding a movie", e);
+		}
+		return null;
+	}	
 }
