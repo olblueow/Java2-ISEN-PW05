cfe3f3751d9ae369825c4b9df2b5b17fa8d8a452
Olivier CLAVIER <clavieroliviermarc@outlook.com>
2026-02-08 13:47:46 +0100

    test: add unit tests for MovieDao

--- src/test/java/fr/isen/java2/db/daos/MovieDaoTestCase.java
+++ src/test/java/fr/isen/java2/db/daos/MovieDaoTestCase.java
@@ -1,14 +1,24 @@
 package fr.isen.java2.db.daos;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import fr.isen.java2.db.entities.Genre;
+import fr.isen.java2.db.entities.Movie;
+
 public class MovieDaoTestCase {
+
+	private final MovieDao movieDao = new MovieDao();
+
 	@BeforeEach
 	public void initDb() throws Exception {
 		Connection connection = DataSourceFactory.getDataSource().getConnection();
@@ -39,16 +49,45 @@ public class MovieDaoTestCase {
 	
 	 @Test
 	 public void shouldListMovies() {
-		 fail("Not yet implemented");
+		// WHEN
+		List<Movie> movies = movieDao.listMovies();
+		// THEN
+		assertThat(movies).hasSize(3);
+		assertThat(movies).extracting("id","title","releaseDate","genre.id","duration","director","summary").containsOnly(
+				tuple(1, "Title 1", LocalDate.parse("2015-11-26"), 1, 120, "director 1", "summary of the first movie"),
+				tuple(2, "My Title 2", LocalDate.parse("2015-11-14"), 2, 114, "director 2", "summary of the second movie"),
+				tuple(3, "Third title", LocalDate.parse("2015-12-12"), 2, 176, "director 3", "summary of the third movie"));
 	 }
 	
 	 @Test
 	 public void shouldListMoviesByGenre() {
-		 fail("Not yet implemented");
+		 // WHEN 
+		 List<Movie> movies = movieDao.listMoviesByGenre("Comedy");
+		 // THEN
+		 assertThat(movies).hasSize(2);
+		 assertThat(movies).extracting("id","title","releaseDate","genre.id","duration","director","summary").containsOnly(
+					tuple(2, "My Title 2", LocalDate.parse("2015-11-14"), 2, 114, "director 2", "summary of the second movie"),
+					tuple(3, "Third title", LocalDate.parse("2015-12-12"), 2, 176, "director 3", "summary of the third movie"));
 	 }
 	
 	 @Test
 	 public void shouldAddMovie() throws Exception {
-		 fail("Not yet implemented");
+		// WHEN
+		movieDao.addMovie(new Movie("New Movie", LocalDate.parse("2015-12-24"), new Genre(1, "Drama"), 142, "new director", "summary of the new movie"));
+		// THEN
+		Connection connection = DataSourceFactory.getDataSource().getConnection();
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM movie WHERE title='New Movie'");
+		assertThat(resultSet.next()).isTrue();
+		assertThat(resultSet.getInt("idmovie")).isNotNull();
+		assertThat(resultSet.getString("title")).isEqualTo("New Movie");
+		assertThat(resultSet.getDate("release_date").toLocalDate()).isEqualTo(LocalDate.parse("2015-12-24"));
+		assertThat(resultSet.getInt("duration")).isEqualTo(142);
+		assertThat(resultSet.getString("director")).isEqualTo("new director");
+		assertThat(resultSet.getString("summary")).isEqualTo("summary of the new movie");
+		assertThat(resultSet.next()).isFalse();
+		resultSet.close();
+		statement.close();
+		connection.close();
 	 }
 }
